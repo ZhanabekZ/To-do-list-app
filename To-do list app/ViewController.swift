205ed37068d48e0e8198a161fa82c1d79d326cfd
ZhanabekZ <zhanabekzinekenov@gmail.com>
2023-08-09 21:15:18 +0600

    Initial Commit

---
+++ To-do list app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  To-do list app
+//
+//  Created by ZhZinekenov on 09.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
